1549d73e0b3d38330e0e0fdaaa4921f94e9951f8
Anurag Pandey <arushpandey123buy@gmail.com>
2022-07-05 02:29:48 +0530

    add

---
+++ app/src/main/java/com/example/bookapp/DashBoardFragment.kt
@@ -0,0 +1,22 @@
+package com.example.bookapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class DashBoardFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_dash_board, container, false)
+    }
+
+
+}
